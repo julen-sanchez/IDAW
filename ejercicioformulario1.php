2bee6176d3c0cd55f4978eb9a8c9f893e7fc3095
Sanchez Alonso <sanchez.julen@uni.lan>
2020-10-13 12:50:07 +0200

    s

---
+++ ejercicioformulario1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Ejemplo de ejercicio sin formulario - ejemplo-sf-1.php
+ *
+ * @author Julen Sanchez
+ *
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Ejemplo de ejercicio sin formulario.
+    Ejemplos.
+    Julen Sanchez
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estiloejercicio.css" title="Color">
+</head>
+
+<body>
+  <h1>Ejemplo de ejercicio con formulario 1</h1>
+
+  <table>
+    <tbody>
+<?php
+
+
+
+?>
+    </tbody>
+  </table>
+
+  <footer>
+    <p>Julen Sanchez</p>
+  </footer>
+</body>
+</html>
