1354836ab3ec7b81f1a8348c2512db48c9099ba4
Sanchez Alonso <sanchez.julen@uni.lan>
2020-10-15 13:44:18 +0200

    s

--- Formularios/respuesta.php
+++ Formularios/respuesta.php
@@ -1,7 +1,55 @@
 <?php
+    $nombre = trim($_REQUEST["Nombre"]);
+
+
     print "<pre>";
     print_r($_REQUEST);
     print "</pre>";
 
-    print "EL NOMBRE ES: $_REQUEST[Nombre]";
+
+
+    if($nombre == ""){
+        print "Nombre no introducido/";
+    }else{
+        print "Nombre: $nombre";
+    }
+
+
+
+    if(isset($_REQUEST["aceptar"])){
+        print "/Has aceptado las condiciones";
+    }
+    else{
+        print "/No has aceptado las condiciones";
+    }
+
+
+
+
+    if(isset($_REQUEST ["ciclos"])){
+        $ciclos = $_REQUEST["ciclos"];
+        if($ciclos == "DAW"){
+            print "/Vas a hacer paginas web";
+        }
+        elseif($ciclos == "DAM"){
+            print "/Vas a hacer videjuegos";
+    }
+    else{
+        print "/Vas montar y administrar bases de datos y redes";
+    }
+
+
+
+if (isset($_REQUEST["intereses2"])) {
+    print "/Tiene interes";
+    $intereses2 = $_REQUEST["intereses2"];
+    foreach($intereses2 as $interes){
+        print "<p>Estas interesado en $interes.</p>";
+    }
+}
+    else{
+        print "/No tiene interes";
+    }
+}
+
 ?>
